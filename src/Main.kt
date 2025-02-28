8369ef49f5ca513db7d8386a7fdbec59ea719605
sebastiandrx <sebastianduenas05@gmail.com>
2025-02-27 21:01:47 -0500

    Acceso_Edificio

--- src/Main.kt
+++ src/Main.kt
@@ -1,45 +1,48 @@
 fun main() {
+        var intentosFallidos = 0
+        val maximoIntentos = 3
 
-        var puntajeDado1 = 0
-        var puntajeDado2 = 0
-        val totalTiradas = 10
+        println("CONTROL DE ACCESO")
 
-        println("JUEGO DE DADOS - 10 TIRADAS ")
-        println("El dado con mayor puntuación total será el ganador.")
-        println()
+        while (intentosFallidos < maximoIntentos) {
+            print("Ingresa tu código de acceso: ")
+            val codigoIngresado = readLine()?.toIntOrNull()
 
-
-        for (tirada in 1..totalTiradas) {
-
-            val resultadoDado1 = (1..6).random()
-            val resultadoDado2 = (1..6).random()
-
-            puntajeDado1 += resultadoDado1
-            puntajeDado2 += resultadoDado2
-
-            println("Tirada #$tirada:")
-            println("Dado 1: $resultadoDado1")
-            println("Dado 2: $resultadoDado2")
-
-            val ganadorTirada = when {
-                resultadoDado1 > resultadoDado2 -> "Dado 1"
-                resultadoDado2 > resultadoDado1 -> "Dado 2"
-                else -> "Empate"
+            if (codigoIngresado == null) {
+                println("Por favor, ingresa un código numérico válido.")
+                continue
             }
-            println("Resultado de la tirada: $ganadorTirada")
-            println()
+
+            if (codigoIngresado in 1000..1999) {
+                println("¡Bienvenido, Empleado #$codigoIngresado!")
+                println("Acceso concedido.")
+                break
+            }
+
+            else if (codigoIngresado in 2000..2999) {
+                println("Bienvenido, Visitante #$codigoIngresado")
+
+                print("Por favor, ingrese su nombre: ")
+                val nombre = readLine() ?: ""
+
+                print("Motivo de la visita: ")
+                val motivo = readLine() ?: ""
+
+                println("Gracias $nombre. Motivo de visita: $motivo")
+                println("Acceso concedido como visitante.")
+                break
+            }
+            else {
+                intentosFallidos++
+                val intentosRestantes = maximoIntentos - intentosFallidos
+
+                if (intentosRestantes > 0) {
+                    println("Código incorrecto. Te quedan $intentosRestantes intentos.")
+                } else {
+                    println("Acceso bloqueado. Haz excedido el número máximo de intentos.")
+                }
+            }
         }
-
-
-        println("=== RESULTADO FINAL ===")
-        println("Puntuación total Dado 1: $puntajeDado1")
-        println("Puntuación total Dado 2: $puntajeDado2")
-
-        val ganadorFinal = when {
-            puntajeDado1 > puntajeDado2 -> "Dado 1"
-            puntajeDado2 > puntajeDado1 -> "Dado 2"
-            else -> "Empate"
-        }
-
-        println("\n¡EL GANADOR ES: $ganadorFinal!")
     }
+
+
